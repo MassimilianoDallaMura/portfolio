677308c7d8f681d4ae91b29027b313aa201fd71a
Massimiliano Dalla Mura <dalla.mura91@gmail.com>
2024-09-13 10:46:51 +0200

    pulizia codice

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -6,51 +6,51 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit, OnDestroy {
+export class HomeComponent {
   private intervalId: any;
 
   constructor(private router: Router) {}
 
-  ngAfterViewInit(): void {
-    this.initCarousel();
-  }
+  // ngAfterViewInit(): void {
+  //   this.initCarousel();
+  // }
 
-  ngOnDestroy(): void {
-    // Pulisce l'intervallo quando il componente viene distrutto
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
+  // ngOnDestroy(): void {
+  //   // Pulisce l'intervallo quando il componente viene distrutto
+  //   if (this.intervalId) {
+  //     clearInterval(this.intervalId);
+  //   }
+  // }
 
-  private initCarousel(): void {
-    const prevButton = document.querySelector('.carousel-control.prev') as HTMLButtonElement;
-    const nextButton = document.querySelector('.carousel-control.next') as HTMLButtonElement;
-    const carouselInner = document.querySelector('.carousel-inner') as HTMLElement;
-    const items = document.querySelectorAll('.carousel-item') as NodeListOf<HTMLElement>;
-    const itemCount = items.length;
-    let currentIndex = 0;
+  // private initCarousel(): void {
+  //   const prevButton = document.querySelector('.carousel-control.prev') as HTMLButtonElement;
+  //   const nextButton = document.querySelector('.carousel-control.next') as HTMLButtonElement;
+  //   const carouselInner = document.querySelector('.carousel-inner') as HTMLElement;
+  //   const items = document.querySelectorAll('.carousel-item') as NodeListOf<HTMLElement>;
+  //   const itemCount = items.length;
+  //   let currentIndex = 0;
 
-    function updateCarousel(): void {
-      const offset = -currentIndex * 100;
-      carouselInner.style.transform = `translateX(${offset}%)`;
-    }
+  //   function updateCarousel(): void {
+  //     const offset = -currentIndex * 100;
+  //     carouselInner.style.transform = `translateX(${offset}%)`;
+  //   }
 
-    prevButton.addEventListener('click', () => {
-      currentIndex = (currentIndex > 0) ? currentIndex - 1 : itemCount - 1;
-      updateCarousel();
-    });
+  //   prevButton.addEventListener('click', () => {
+  //     currentIndex = (currentIndex > 0) ? currentIndex - 1 : itemCount - 1;
+  //     updateCarousel();
+  //   });
 
-    nextButton.addEventListener('click', () => {
-      currentIndex = (currentIndex < itemCount - 1) ? currentIndex + 1 : 0;
-      updateCarousel();
-    });
+  //   nextButton.addEventListener('click', () => {
+  //     currentIndex = (currentIndex < itemCount - 1) ? currentIndex + 1 : 0;
+  //     updateCarousel();
+  //   });
 
-    // Auto slide ogni 5 sec
-    this.intervalId = setInterval(() => {
-      currentIndex = (currentIndex < itemCount - 1) ? currentIndex + 1 : 0;
-      updateCarousel();
-    }, 5000);
-  }
+  //   // Auto slide ogni 5 sec
+  //   this.intervalId = setInterval(() => {
+  //     currentIndex = (currentIndex < itemCount - 1) ? currentIndex + 1 : 0;
+  //     updateCarousel();
+  //   }, 5000);
+  // }
 
   navigateToCertifications(): void {
     this.router.navigate(['/bio'], { fragment: 'certifications' }).then(() => {
